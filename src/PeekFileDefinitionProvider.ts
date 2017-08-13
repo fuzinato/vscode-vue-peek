1d39a3d846e372737e98af5ea2f09444bd992f93
Dario <fussinatto@gmail.com>
2017-08-13 23:43:43 +0200

    split code to modules

---
+++ src/PeekFileDefinitionProvider.ts
@@ -0,0 +1,60 @@
+import * as vscode from 'vscode';
+
+export default class PeekFileDefinitionProvider implements vscode.DefinitionProvider {
+  protected targetFileExtensions: string[] = [];
+
+  constructor(targetFileExtensions: string[] = []) {
+    this.targetFileExtensions = targetFileExtensions;
+  }
+
+  getComponentName(position: vscode.Position): String[] {
+    const doc = vscode.window.activeTextEditor.document;
+    const selection = doc.getWordRangeAtPosition(position);
+    const selectedText = doc.getText(selection);
+    let possibleFileNames = [],
+      altName = ''
+
+    selectedText.match(/\w+/g).forEach(str => {
+      return altName += str[0].toUpperCase() + str.substring(1);
+    })
+
+    this.targetFileExtensions.forEach(ext => {
+      possibleFileNames.push(selectedText + ext)
+      possibleFileNames.push(altName + ext)
+    })
+
+    return possibleFileNames;
+  }
+
+  searchFilePath(fileName: String): Thenable<vscode.Uri[]> {
+    return vscode.workspace.findFiles(`**/${fileName}`, '**/node_modules'); // Returns promise
+  }
+
+  provideDefinition(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken
+  ): Promise<vscode.Location | vscode.Location[]> {
+    
+    let filePaths = [];
+    const componentNames = this.getComponentName(position, );
+    const searchPathActions = componentNames.map(this.searchFilePath);
+    const searchPromises = Promise.all(searchPathActions); // pass array of promises
+    
+    return searchPromises.then((paths) => {
+      filePaths = [].concat.apply([], paths);
+      
+      if (filePaths.length) {
+        let allPaths = [];
+        filePaths.forEach(filePath => {
+          allPaths.push(new vscode.Location(vscode.Uri.file(`${filePath.path}`),new vscode.Position(0,1) ))
+        });
+        return allPaths;
+      } else {
+        return undefined;
+      }
+    }, (reason) => {
+      return undefined;
+    }); 
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,17 @@
 'use strict';
 import * as vscode from 'vscode';
-// import { TextDocument, Position, CancellationToken, CompletionItem, languages } from 'vscode';
+import PeekFileDefinitionProvider from './PeekFileDefinitionProvider'
 
 const languageConfiguration: vscode.LanguageConfiguration = {
   wordPattern: /(\w+((-\w+)+)?)/
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  vscode
 
   const configParams = vscode.workspace.getConfiguration('vue-peek');
   const supportedLanguages = configParams.get('supportedLanguages') as Array<string>;
   const targetFileExtensions = configParams.get('targetFileExtensions') as Array<string>;
-
+  
   context.subscriptions.push(vscode.languages.registerDefinitionProvider(
     supportedLanguages,
     new PeekFileDefinitionProvider(targetFileExtensions)
@@ -26,60 +25,4 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-}
-
-class PeekFileDefinitionProvider implements vscode.DefinitionProvider {
-  protected targetFileExtensions: string[] = [];
-
-  constructor(targetFileExtensions: string[] = []) {
-    this.targetFileExtensions = targetFileExtensions;
-  }
-
-  getComponentName(position: vscode.Position): String[] {
-    const doc = vscode.window.activeTextEditor.document;
-    const selection = doc.getWordRangeAtPosition(position);
-    const selectedText = doc.getText(selection);
-    let possibleFileNames = [],
-      altName = ''
-
-    selectedText.match(/\w+/g).forEach(str => {
-      return altName += str[0].toUpperCase() + str.substring(1);
-    })
-
-    possibleFileNames.push(altName, selectedText);
-
-    return possibleFileNames;
-  }
-
-  searchFilePath(fileName: String): Thenable<vscode.Uri[]> {
-    return vscode.workspace.findFiles(`**/${fileName}.vue`, '**/node_modules'); // Returns promise
-  }
-
-  provideDefinition(
-    document: vscode.TextDocument,
-    position: vscode.Position,
-    token: vscode.CancellationToken
-  ): Promise<vscode.Location | vscode.Location[]> {
-    
-    let filePaths = [];
-    const componentNames = this.getComponentName(position);
-    const searchPathActions = componentNames.map(this.searchFilePath);
-    const searchPromises = Promise.all(searchPathActions); // pass array of promises
-    
-    return searchPromises.then((paths) => {
-      filePaths = [].concat.apply([], paths);
-      
-      if (filePaths.length) {
-        let allPaths = [];
-        filePaths.forEach(filePath => {
-          allPaths.push(new vscode.Location(vscode.Uri.file(`${filePath.path}`),new vscode.Position(0,1) ))
-        });
-        return allPaths;
-      } else {
-        return undefined;
-      }
-    }, (reason) => {
-      return undefined;
-    }); 
-  }
 }
